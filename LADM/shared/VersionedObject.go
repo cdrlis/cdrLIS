738111d11439c5186a8ad49a302d2bdbb85769b1
Mario Vuzem <mario.vuzem1@hotmail.com>
2020-03-29 21:36:53 +0200

    Update versioned object

--- LADM/shared/VersionedObject.go
+++ LADM/shared/VersionedObject.go
@@ -2,7 +2,14 @@ package shared
 
 // VersionedObject Versioned object
 type VersionedObject struct {
-	ID                   string
 	BeginLifespanVersion string
-	EndLifespanVersion   string
+	EndLifespanVersion   *string
+	Quality              *DQElement
+	Source               *CIResponsibleParty
 }
+
+// DQElement DQ Elemenet
+type DQElement string // TODO external package
+
+// CIResponsibleParty CI Responsible party
+type CIResponsibleParty string // TODO external package

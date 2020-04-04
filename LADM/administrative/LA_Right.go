b52f1b6527e05e88136f250167545210490568f3
zgalic <galic.zdravko@chello.at>
2020-04-04 18:28:26 +0200

    Added LADM comment and completed code list LA_RightType

--- LADM/administrative/LA_Right.go
+++ LADM/administrative/LA_Right.go
@@ -1,9 +1,13 @@
 package administrative
 
-// LARight Right
+//
+// Administrative::LA_Right
+//
+// An instance of class LA_Right is a right. LA_Right is a subclass of class LA_RRR. A right may be associated
+// to zero or more [0..*] mortgages (i.e. a mortgage is associated to the affected basic administrative unit but it
+// may also be specifically associated to the right which is the object of the mortgage); see Figure 10.
 type LARight struct {
 	LARRR
-
 	Type LARightType
 }
 
@@ -12,5 +16,35 @@ type LARightType int
 
 const (
 	// DefaultRight Default right type
-	DefaultRight LARightType = 0
+	DefaultRight LARightType = iota
+	AgriActivity
+	BelowTheDepth
+	BoatHarbour
+	CommonwealthAcquisition
+	Covenant
+	Easement
+	ExcludedArea
+	Forest
+	Freeholding
+	Grazing
+	HousingLand
+	IndustrialState
+	LandsLease
+	Lease
+	MainRoad
+	MarinePark
+	MineTenure
+	NationalPark
+	Occupation
+	Ownership
+	PortAuthority
+	ProfitPrendre
+	Railway
+	Reserve
+	StateForest
+	StateLand
+	TimberReserve
+	TransferredProperty
+	WaterResource
+	WaterRights
 )

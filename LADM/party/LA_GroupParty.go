82e7fe81b839af6b7cb7c09ecedf665e57daae26
zgalic <galic.zdravko@chello.at>
2020-04-04 17:44:09 +0200

    Completed code lists

--- LADM/party/LA_GroupParty.go
+++ LADM/party/LA_GroupParty.go
@@ -2,10 +2,17 @@ package party
 
 import "github.com/cdrlis/cdrLIS/LADM/common"
 
-// LAGroupParty Group party
+//
+// Party::LA_GroupParty
+//
+// An instance of class LA_GroupParty is a group party. Class LA_GroupParty is a subclass of LA_Party, thus
+// allowing instances of class LA_GroupParty to have an association with instances of class LA_RRR (and
+// thereby also to class LA_BAUnit). A group party consists of two or more [2..*] parties, but also of other group
+// parties (that is to say, a group party of group parties). Conversely, a party is a member of zero or more [0..*]
+// group parties, see Figure 9.
+
 type LAGroupParty struct {
 	LAParty
-
 	GroupID common.Oid
 	Type    LAGroupPartyType
 	Parties []LAPartyMember
@@ -16,5 +23,9 @@ type LAGroupPartyType int
 
 const (
 	// DefaultGroupParty Default Group party type
-	DefaultGroupParty LAGroupPartyType = 0
+	DefaultGroupParty LAGroupPartyType = iota
+	Association
+	BAUnitGroup
+	Family
+	Tribe
 )

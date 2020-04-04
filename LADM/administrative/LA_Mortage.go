7885dc80ab011105443741f9ed20461b482cb352
zgalic <galic.zdravko@chello.at>
2020-04-04 19:20:28 +0200

    Added LADM comment and completed code list LAMortgageType

--- LADM/administrative/LA_Mortage.go
+++ LADM/administrative/LA_Mortage.go
@@ -1,22 +1,48 @@
 package administrative
 
-// LAMortage Mortage
-type LAMortage struct {
-	LARestriction
+//
+// Administrative::LA_Mortgage
+//
+// An instance of class LA_Mortgage is a mortgage. LA_Mortgage is a subclass of LA_Restriction. LA_Mortgage
+// is associated to class LA_Right (the right that is the basis for the mortgage). A mortgage can be associated to
+// zero or more [0..*] rights (i.e. a mortgage can be associated specifically to the right which is the object of the
+// mortgage). In all cases, the mortgage is associated, through LA_Restriction and LA_RRR, to the basic
+//administrative unit which is affected by the mortgage; see Figure 10.
 
+type LAMortgage struct {
+	LARestriction
 	Amount       *Currency
-	InterestRate *float64
+	InterestRate *float32
 	Ranking      *int
-	Type         LAMortageType
+	Type         *LAMortgageType
 }
 
-// Currency Currency
-type Currency string
+type Currency struct {
+	Amount float32
+	Code   iso4217
+}
+
+// Currency based on ISO 4217
+type iso4217 int
+
+const (
+	// DefaultISO4217 Default currency type
+	DefaultISO4217 iso4217 = iota
+	AED
+	// ...
+	EUR
+	// ...
+	USD
+	// ..
+)
 
 // LAMortageType Mortage type
-type LAMortageType int
+type LAMortgageType int
 
 const (
 	// DefaultMortage Default mortage type
-	DefaultMortage LAMortageType = 0
+	DefaultMortgage LAMortgageType = iota
+	LevelPayment
+	Linear
+	Microcredit
 )

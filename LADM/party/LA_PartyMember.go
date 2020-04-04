2b45d9edf16c9c0d61b62bbe4230230c1f82710e
zgalic <galic.zdravko@chello.at>
2020-04-04 18:02:05 +0200

    Added LADM comment

--- LADM/party/LA_PartyMember.go
+++ LADM/party/LA_PartyMember.go
@@ -2,10 +2,13 @@ package party
 
 import "github.com/cdrlis/cdrLIS/LADM/common"
 
-// LAPartyMember Party member
+//
+// Party::LA_PartyMember
+//
+// An instance of class LA_PartyMember is a party member. Class LA_PartyMember is an optional association
+// class between LA_Party and LA_GroupParty, see Figure 9.
 type LAPartyMember struct {
 	common.VersionedObject
-
 	Share *common.Fraction
 	Party *LAParty
 	Group *LAGroupParty

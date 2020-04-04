76ae88f1ddcc63177e143b8704e951ab596fbead
zgalic <galic.zdravko@chello.at>
2020-04-04 17:47:01 +0200

    Updated and added LADM comment

--- LADM/common/VersionedObject.go
+++ LADM/common/VersionedObject.go
@@ -5,10 +5,13 @@ import (
 	"time"
 )
 
-// VersionedObject Versioned object
+// Type VersionedObject is introduced in the LADM to manage and maintain historical data in the database.
+// History requires, that inserted and superseded data, are given a time-stamp. In this way, the contents of the
+// database can be reconstructed, as they were at any historical moment. For more on history and dynamic
+// aspects of LA systems, see Annex N.
 type VersionedObject struct {
 	BeginLifespanVersion time.Time
-	EndLifespanVersion   time.Time
-	Quality              metadata.DQ_Element
-	Source               metadata.CI_ResponsibleParty
+	EndLifespanVersion   *time.Time
+	Quality              *metadata.DQ_Element
+	Source               *metadata.CI_ResponsibleParty
 }

cb4686e09607bdb3c6ec607d73ae4d044140318a
zgalic <galic.zdravko@chello.at>
2020-04-04 18:34:42 +0200

    Added LADM comment and completed code list LARestrictionType

--- LADM/administrative/LA_Restriction.go
+++ LADM/administrative/LA_Restriction.go
@@ -1,9 +1,13 @@
 package administrative
 
-// LARestriction Restriction
+//
+// Administrative::LA_Restriction
+//
+// An instance of class LA_Restriction is a restriction. LA_Restriction is a subclass of class LA_RRR.
+// LA_Mortgage is a specialization of LA_Restriction (6.4.6); see Figure 10.
+
 type LARestriction struct {
 	LARRR
-
 	PartyRequired *bool
 	Type          LARestrictionType
 }
@@ -13,5 +17,11 @@ type LARestrictionType int
 
 const (
 	// DefaultRestriction Default Restriction type
-	DefaultRestriction LARestrictionType = 0
+	DefaultRestriction LARestrictionType = iota
+	AdminPublicServitude
+	Monument
+	MonumentPartly
+	NoBuilding
+	Servitude
+	ServitudePartly
 )

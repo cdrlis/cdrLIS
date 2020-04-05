ff240073bb2bbfcc93a734a2ecbf726d4709ed91
zgalic <galic.zdravko@chello.at>
2020-04-05 12:48:10 +0200

    Added filed VersionedObject and LADM comment

--- LADM/spatialunit/LA_SpatialUnitGroup.go
+++ LADM/spatialunit/LA_SpatialUnitGroup.go
@@ -1,9 +1,21 @@
 package spatialunit
 
-import "github.com/cdrlis/cdrLIS/LADM/common"
+import (
+	"github.com/cdrlis/cdrLIS/LADM/common"
+	"github.com/paulsmith/gogeos/geos"
+)
 
-// LASpatialUnitGroup Spatial unit group
+//
+// Spatial Unit::LA_SpatialUnitGroup
+//
+// Any number of spatial units (4.1.23), considered as an entity.
+// An instance of class LA_SpatialUnitGroup is a spatial unit group. A spatial unit group is made of one or more
+// [1..*] parts/elements (which can be spatial units, or spatial unit groups, or a combination of spatial units and
+// spatial unit groups). A spatial unit group is part of zero or one [0..1] larger spatial unit group, which again can
+// even be part of zero or one [0..1] larger spatial unit group, and so on. See Figure 11.
+
 type LASpatialUnitGroup struct {
+	common.VersionedObject
 	HierarchyLevel int
 	Label          *string
 	Name           *string

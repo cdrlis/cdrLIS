b52eeb123e166b7a07c4e465be7cef6412d2ee5f
zgalic <galic.zdravko@chello.at>
2020-04-05 14:39:15 +0200

    Updated

--- LADM/common/metadata/CI_ResponsibleParty.go
+++ LADM/common/metadata/CI_ResponsibleParty.go
@@ -7,19 +7,19 @@ import (
 )
 
 type CI_Citation struct {
-	Title          string
-	AlltemateTitle string
-	date           CI_Date
-	edition        string
-	editionDate    time.Time
+	Title         string
+	AltemateTitle *string
+	Date          []CI_Date
+	Edition       *string
+	EditionDate   *time.Time
 	//	Identifier				MD_Identifier
-	CitedResponsibleParty CI_ResponsibleParty
-	PresentationForm      CI_PresentationFormCode
-	Series                CI_Series
-	OtherCitationDetails  string
-	CollectiveTitle       string
-	ISBN                  string
-	ISSN                  string
+	CitedResponsibleParty *CI_ResponsibleParty
+	PresentationForm      *CI_PresentationFormCode
+	Series                *CI_Series
+	OtherCitationDetails  *string
+	CollectiveTitle       *string
+	ISBN                  *string
+	ISSN                  *string
 }
 
 type CI_Date struct {
@@ -36,10 +36,10 @@ const (
 )
 
 type CI_ResponsibleParty struct {
-	IndividualName   string
-	OrganizationName string
-	PositionName     string
-	ContactInfo      CI_Contact
+	IndividualName   *string
+	OrganizationName *string
+	PositionName     *string
+	ContactInfo      *CI_Contact
 	Role             CI_RoleCode
 }
 
@@ -57,12 +57,12 @@ type CI_Telephone struct {
 }
 
 type CI_Address struct {
-	DeliveryPoint         string
+	DeliveryPoint         []string
 	City                  string
 	AdministrativeArea    string
 	PostalCode            string
 	Country               string
-	ElectronicMailAddress mail.Address
+	ElectronicMailAddress []mail.Address
 }
 
 type CI_RoleCode int

c1ce1facf7e28d2241ff5e033d344e6251a1a83f
zgalic <galic.zdravko@chello.at>
2020-04-11 12:47:07 +0200

    Renamed methods

--- logic/IRepository.go
+++ logic/IRepository.go
@@ -3,8 +3,8 @@ package logic
 // IRepository CRUD
 type IRepository interface {
 	Create(value interface{}) error
-	Get(out interface{}) error
-	GetAll(out interface{}) error
+	Read(out interface{}) error
+	ReadAll(out interface{}) error
 	Update(value interface{}) error
 	Delete(value interface{}) error
 }

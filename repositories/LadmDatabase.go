8f0dede296774d332f60c24738f845363f6c8ee2
zgalic <galic.zdravko@chello.at>
2020-04-11 13:09:35 +0200

    Changed type name

--- repositories/LadmDatabase.go
+++ repositories/LadmDatabase.go
@@ -2,31 +2,31 @@ package repositories
 
 import "github.com/jinzhu/gorm"
 
-type LadmRepository struct {
+type LadmDatabase struct {
 	DB *gorm.DB
 }
 
-func (ctx LadmRepository) Create(value interface{}) error {
+func (ctx LadmDatabase) Create(value interface{}) error {
 	ctx.DB.Create(value)
 	return nil // TODO error handling
 }
 
-func (ctx LadmRepository) Read(out interface{}) error {
+func (ctx LadmDatabase) Read(out interface{}) error {
 	ctx.DB.First(out)
 	return nil // TODO error handling
 }
 
-func (ctx LadmRepository) ReadAll(out interface{}) error {
+func (ctx LadmDatabase) ReadAll(out interface{}) error {
 	ctx.DB.Find(out)
 	return nil // TODO error handling
 }
 
-func (ctx LadmRepository) Update(value interface{}) error {
+func (ctx LadmDatabase) Update(value interface{}) error {
 	ctx.DB.Save(value)
 	return nil // TODO error handling
 }
 
-func (ctx LadmRepository) Delete(value interface{}) error {
+func (ctx LadmDatabase) Delete(value interface{}) error {
 	ctx.DB.Delete(value)
 	return nil // TODO error handling
 }

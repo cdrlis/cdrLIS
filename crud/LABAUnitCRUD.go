371ba35b1837417d87e5309c1c59700a2453d669
Mario Vuzem <mario.vuzem1@hotmail.com>
2020-06-12 11:04:56 +0200

    Bugfix

--- crud/LABAUnitCRUD.go
+++ crud/LABAUnitCRUD.go
@@ -15,14 +15,10 @@ func (crud LABAUnitCRUD) Read(where ...interface{}) (interface{}, error) {
 	var baUnit ladm.LABAUnit
 	if where != nil {
 		reader := crud.DB.Where("uid = ?::\"Oid\" AND endlifespanversion IS NULL", where).
-			Preload("Rights", "endlifespanversion IS NULL").
-			Preload("Rights.Party", "endlifespanversion IS NULL").
-			Preload("Responsibilities", "endlifespanversion IS NULL").
-			Preload("Responsibilities.Party", "endlifespanversion IS NULL").
-			Preload("Restrictions", "endlifespanversion IS NULL").
-			Preload("Restrictions.Party", "endlifespanversion IS NULL").
-			Preload("Su", "endlifespanversion IS NULL").
-			Preload("Su.Su", "endlifespanversion IS NULL").
+			Preload("RRR", "endlifespanversion IS NULL").
+			Preload("RRR.Party", "endlifespanversion IS NULL").
+			Preload("SU", "endlifespanversion IS NULL").
+			Preload("SU.SU", "endlifespanversion IS NULL").
 			First(&baUnit)
 		if reader.RowsAffected == 0 {
 			return nil, errors.New("Entity not found")
--- crud/LARRRCRUD.go
+++ crud/LARRRCRUD.go
@@ -34,7 +34,7 @@ func (crud LARRRCRUD) Create(rrrIn interface{}) (interface{}, error) {
 	tx := crud.DB.Begin()
 	rrr := rrrIn.(ladm.LARRR)
 	existing := 0
-	reader := tx.Model(&ladm.LARRR{}).Where("rid = ?::\"Oid\" AND endlifespanversion IS NULL", rrr.RID.String()).
+	reader := tx.Model(&ladm.LARRR{}).Where("rid = ?::\"Oid\" AND endlifespanversion IS NULL", rrr.RID).
 		Count(&existing)
 	if reader.Error != nil{
 		tx.Rollback()
@@ -174,65 +174,51 @@ func (crud LARRRCRUD) Update(rrrIn interface{}) (interface{}, error) {
 	rrr.ID = rrr.RID.String()
 	rrr.BeginLifespanVersion = currentTime
 	rrr.EndLifespanVersion = nil
+	rrr.PartyID = oldRrr.PartyID
+	rrr.PartyBeginLifespanVersion = oldRrr.PartyBeginLifespanVersion
+	rrr.UnitID = oldRrr.UnitID
+	rrr.UnitBeginLifespanVersion = oldRrr.UnitBeginLifespanVersion
 	writer = tx.Set("gorm:save_associations", false).Create(&rrr)
 	if writer.Error != nil {
 		tx.Rollback()
 		return nil, writer.Error
 	}
-	reader = tx.Where("lid = ?::\"Oid\" AND endlifespanversion = ?", rrr.RID, currentTime).
+	reader = tx.Where("rid = ?::\"Oid\" AND endlifespanversion = ?", rrr.RID, currentTime).
 		Preload("Right", "endlifespanversion IS NULL").
 		Preload("Responsibility", "endlifespanversion IS NULL").
 		Preload("Restriction", "endlifespanversion IS NULL").
 		Preload("Restriction.Mortgage", "endlifespanversion IS NULL").
 		First(&oldRrr)
+	if reader.Error != nil{
+		return nil, reader.Error
+	}
 	if reader.RowsAffected == 0 {
 		tx.Rollback()
 		return nil, errors.New("Entity not found")
 	}
 
-	if right := rrr.Right; right != nil {
+	if right := oldRrr.Right; right != nil {
 		right.EndLifespanVersion = &currentTime
 		writer = tx.Set("gorm:save_associations", false).Save(&right)
 		if writer.RowsAffected == 0 {
 			tx.Rollback()
 			return nil, errors.New("Entity not found")
 		}
-		right.BeginLifespanVersion = currentTime
-		right.EndLifespanVersion = nil
-		writer = tx.Set("gorm:save_associations", false).Create(right)
-		if writer.Error != nil {
-			tx.Rollback()
-			return nil, writer.Error
-		}
 	}
-	if responsibility := rrr.Responsibility; responsibility != nil {
+	if responsibility := oldRrr.Responsibility; responsibility != nil {
 		responsibility.EndLifespanVersion = &currentTime
 		writer = tx.Set("gorm:save_associations", false).Save(&responsibility)
 		if writer.RowsAffected == 0 {
 			tx.Rollback()
 			return nil, errors.New("Entity not found")
 		}
-		responsibility.BeginLifespanVersion = currentTime
-		responsibility.EndLifespanVersion = nil
-		writer = tx.Set("gorm:save_associations", false).Create(responsibility)
-		if writer.Error != nil {
-			tx.Rollback()
-			return nil, writer.Error
-		}
 	}
-	if restriction := rrr.Restriction; restriction != nil {
+	if restriction := oldRrr.Restriction; restriction != nil {
 		restriction.EndLifespanVersion = &currentTime
 		writer = tx.Set("gorm:save_associations", false).Save(&restriction)
 		if writer.RowsAffected == 0 {
 			tx.Rollback()
 			return nil, errors.New("Entity not found")
-		}
-		restriction.BeginLifespanVersion = currentTime
-		restriction.EndLifespanVersion = nil
-		writer = tx.Set("gorm:save_associations", false).Create(restriction)
-		if writer.Error != nil {
-			tx.Rollback()
-			return nil, writer.Error
 		}
 		if mortgage := restriction.Mortgage; mortgage != nil{
 			mortgage.EndLifespanVersion = &currentTime
@@ -241,8 +227,46 @@ func (crud LARRRCRUD) Update(rrrIn interface{}) (interface{}, error) {
 				tx.Rollback()
 				return nil, errors.New("Entity not found")
 			}
-			mortgage.BeginLifespanVersion = currentTime
-			mortgage.EndLifespanVersion = nil
+		}
+	}
+
+	if right := rrr.Right; right != nil {
+		right.ID = rrr.ID
+		right.RID = rrr.RID
+		right.BeginLifespanVersion = rrr.BeginLifespanVersion
+		right.EndLifespanVersion = rrr.EndLifespanVersion
+		writer = tx.Set("gorm:save_associations", false).Create(right)
+		if writer.Error != nil {
+			tx.Rollback()
+			return nil, writer.Error
+		}
+	}
+	if responsibility := rrr.Responsibility; responsibility != nil {
+		responsibility.ID = rrr.ID
+		responsibility.RID = rrr.RID
+		responsibility.BeginLifespanVersion = rrr.BeginLifespanVersion
+		responsibility.EndLifespanVersion = rrr.EndLifespanVersion
+		writer = tx.Set("gorm:save_associations", false).Create(responsibility)
+		if writer.Error != nil {
+			tx.Rollback()
+			return nil, writer.Error
+		}
+	}
+	if restriction := rrr.Restriction; restriction != nil {
+		restriction.ID = rrr.ID
+		restriction.RID = rrr.RID
+		restriction.BeginLifespanVersion = rrr.BeginLifespanVersion
+		restriction.EndLifespanVersion = rrr.EndLifespanVersion
+		writer = tx.Set("gorm:save_associations", false).Create(restriction)
+		if writer.Error != nil {
+			tx.Rollback()
+			return nil, writer.Error
+		}
+		if mortgage := restriction.Mortgage; mortgage != nil{
+			mortgage.ID = rrr.ID
+			mortgage.RID = rrr.RID
+			mortgage.BeginLifespanVersion = rrr.BeginLifespanVersion
+			mortgage.EndLifespanVersion = rrr.EndLifespanVersion
 			writer = tx.Set("gorm:save_associations", false).Create(mortgage)
 			if writer.Error != nil {
 				tx.Rollback()
